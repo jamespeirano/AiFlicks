05fb71e74878bf5f6e1f593ea79014a495410fdc
Izhar Ali <aliizh94@students.rowan.edu>
2023-08-27 15:06:00 -0400

    new changes

--- app/data.py
+++ app/data.py
@@ -48,8 +48,14 @@ MODELS = [
         'is_free': True,
     },
     {
+        'title': 'Dream Portray XL Base',
+        'name' : 'stable-diffusion-xl-base-0.9',
+        'description': 'Experience the magic where dreams meet reality. Dream Portray renders your imagination into dreamlike images, adding a touch of photorealistic charm.',
+        'is_free': True,
+    },
+    {
         'title': 'Next Gen Render XL Base',
-        'name' : 'stable-diffusion_xl_base',
+        'name' : 'stable-diffusion-xl-base-1.0',
         'description': 'Step into the future of image rendering with Next Gen Render. Your wildest imaginations seamlessly become stunning, photorealistic masterpieces.',
         'is_free': True,
     },
